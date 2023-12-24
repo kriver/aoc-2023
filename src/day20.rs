20c50d45400a61a0e5283908fc450a608dc32d8f
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2023-12-24 18:36:50 +0100

    day 20 part 1, part 2 WIP

--- src/day20.rs
+++ src/day20.rs
@@ -1,210 +1,294 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::HashMap, fmt::Display};
+
+use itertools::Itertools;
 
 use crate::util::load;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum PulseType {
-    LOW,
-    HIGH,
+fn set_bit(state: u64, bit_mask: u64, value: bool) -> u64 {
+    match value {
+        true => state | bit_mask,
+        false => state & !bit_mask,
+    }
 }
 
 #[derive(Debug)]
-struct Pulse<'a> {
-    pt: PulseType,
-    src: &'a str,
-    dst: &'a str,
+struct Pulse {
+    src_mask: u64,
+    dst_mask: u64,
+    high: bool,
+}
+
+impl Pulse {
+    fn new(src_mask: u64, dst_mask: u64, high: bool) -> Self {
+        Pulse {
+            src_mask,
+            dst_mask,
+            high,
+        }
+    }
+}
+
+impl Display for Pulse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} --[{}]--> {}",
+            self.src_mask,
+            if self.high { 'H' } else { 'L' },
+            self.dst_mask,
+        )
+    }
 }
 
 #[derive(Debug)]
 enum ModuleType {
-    FlipFlop,
-    Conjunction,
+    FlipFlop(bool),
+    Conjunction(u64),
     Broadcaster,
+    Rx(usize),
+}
+
+impl Display for ModuleType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ModuleType::FlipFlop(state) => write!(f, "FF({})", if *state { "on" } else { "off" }),
+            ModuleType::Conjunction(state) => write!(f, "CJ({})", state),
+            ModuleType::Broadcaster => write!(f, "BC"),
+            ModuleType::Rx(cnt) => write!(f, "RX({})", cnt),
+        }
+    }
 }
 
 #[derive(Debug)]
 struct Module {
     name: String,
-    outputs: Vec<String>,
+    id_mask: u64,
     mt: ModuleType,
+    src_mask: u64,
+    dst: Vec<u64>,
+}
+
+type Modules = HashMap<u64, Module>;
+
+impl Display for Module {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "* {} = {}", self.name, self.mt)
+    }
 }
 
 impl Module {
-    fn pulse(&self, state: &mut State, pulse: Pulse) -> Vec<Pulse> {
-        let out = match self.mt {
-            ModuleType::FlipFlop => match pulse.pt {
-                PulseType::LOW => {
-                    if state.flip_state(&self.name) {
-                        Some(PulseType::HIGH)
-                    } else {
-                        Some(PulseType::LOW)
-                    }
-                }
-                PulseType::HIGH => None,
-            },
-            ModuleType::Conjunction => {
-                state.set_state(pulse.src, pulse.pt == PulseType::HIGH);
-                let all_high = state.are_inputs_all_high(&self.name);
-                if all_high {
-                    Some(PulseType::LOW)
+    fn receive(&mut self, pulse: &Pulse) -> Vec<Pulse> {
+        // println!(
+        //     "\t{:?}({}) (src={}, dst={})",
+        //     self.name, self.id_mask, pulse.src_mask, pulse.dst_mask
+        // );
+        let high = match self.mt {
+            ModuleType::FlipFlop(state) => {
+                if pulse.high {
+                    // println!("\t`-> with state {}", state);
+                    None
                 } else {
-                    Some(PulseType::HIGH)
+                    // println!("\t`-> with state {} -> {}", state, !state);
+                    self.mt = ModuleType::FlipFlop(!state);
+                    Some(!state)
                 }
             }
-            ModuleType::Broadcaster => Some(pulse.pt),
+            ModuleType::Conjunction(state) => {
+                let new_state = set_bit(state, pulse.src_mask, pulse.high);
+                // println!("\t`-> with state {} -> {}", state, new_state);
+                self.mt = ModuleType::Conjunction(new_state);
+                if new_state & self.src_mask == self.src_mask {
+                    Some(false)
+                } else {
+                    Some(true)
+                }
+            }
+            ModuleType::Broadcaster => Some(pulse.high),
+            ModuleType::Rx(cnt) => {
+                let c = if pulse.high { cnt } else { cnt + 1 };
+                self.mt = ModuleType::Rx(c);
+                None
+            }
         };
-        match out {
-            Some(pt) => self
-                .outputs
+        high.map(|h| {
+            self.dst
                 .iter()
-                .map(|o| Pulse {
-                    pt,
-                    src: self.name.as_str(),
-                    dst: o,
+                .map(|d| Pulse::new(self.id_mask, *d, h))
+                .collect_vec()
+        })
+        .unwrap_or(vec![])
+    }
+}
+
+fn input(file: &str) -> (u64, u64, Modules) {
+    let mods = load::<String>(file)
+        .into_iter()
+        .map(|l| l.split(" -> ").map(|s| s.to_string()).collect_vec())
+        .map(|tokens| {
+            let (name, mt) = match tokens[0].chars().next().unwrap() {
+                '%' => (tokens[0][1..].to_string(), ModuleType::FlipFlop(false)),
+                '&' => (tokens[0][1..].to_string(), ModuleType::Conjunction(0)),
+                _ => (tokens[0].to_string(), ModuleType::Broadcaster),
+            };
+            (name, mt, tokens[1].to_string())
+        })
+        .collect_vec();
+    // map names to bits
+    let mut max_bit = 0;
+    let mut name2bit = mods
+        .iter()
+        .enumerate()
+        .map(|(i, (name, _, _))| {
+            let b = 1u64 << i;
+            max_bit = max_bit.max(b);
+            (name.to_string(), b)
+        })
+        .collect::<HashMap<_, _>>();
+    let mut dst2src: HashMap<u64, u64> = HashMap::new();
+    // create modules
+    let mut rx = None;
+    let mut modules: HashMap<_, _> = mods
+        .into_iter()
+        .map(|(name, mt, dst_lst)| {
+            let id_mask = *name2bit.get(&name).unwrap();
+            let dst = dst_lst
+                .split(", ")
+                .map(|n| {
+                    // let dst_mask = *name2bit.get(&n.to_string()).unwrap_or(&(max_bit << 1));
+                    let dst_mask = name2bit.entry(n.to_string()).or_insert_with(|| {
+                        max_bit <<= 1;
+                        rx = Some(max_bit);
+                        max_bit
+                    });
+                    dst2src
+                        .entry(*dst_mask)
+                        .and_modify(|d| *d |= id_mask)
+                        .or_insert(id_mask);
+                    *dst_mask
                 })
-                .collect(),
-            None => vec![],
-        }
+                .collect_vec();
+            (
+                id_mask,
+                Module {
+                    name,
+                    id_mask,
+                    mt,
+                    src_mask: 0,
+                    dst,
+                },
+            )
+        })
+        .collect();
+    // Add Rx
+    rx.iter().for_each(|id| {
+        modules.insert(
+            *id,
+            Module {
+                name: "rx".to_string(),
+                id_mask: *id,
+                mt: ModuleType::Rx(0),
+                src_mask: 0,
+                dst: vec![],
+            },
+        );
+    });
+    // init sources
+    for m in modules.values_mut() {
+        dst2src
+            .get(&m.id_mask)
+            .iter()
+            .for_each(|d| m.src_mask = **d);
     }
+    (
+        *name2bit.get("broadcaster").unwrap(),
+        rx.unwrap_or(0),
+        modules,
+    )
 }
 
-#[derive(Debug)]
-struct State {
-    s: HashMap<String, u64>,
-}
-
-impl State {
-    fn bitmask(&self, name: &str) -> u64 {
-        let id = self.ids.get(name).unwrap();
-        1 << id
-    }
-
-    fn get_state(&self, name: &str) -> bool {
-        self.state & self.bitmask(name) != 0
-    }
-
-    fn set_state(&mut self, name: &str, high: bool) {
-        if high {
-            self.state |= self.bitmask(name);
-        } else {
-            self.state &= !self.bitmask(name);
+fn push_button(mods: &mut Modules, broadcaster: u64) -> (usize, usize) {
+    let (mut lo, mut hi) = (0, 0);
+    let initial = Pulse::new(9999, broadcaster, false);
+    let mut pulses = vec![initial];
+    loop {
+        if pulses.is_empty() {
+            break;
         }
-    }
-
-    fn flip_state(&mut self, name: &str) -> bool {
-        let bitmask = self.bitmask(name);
-        self.state ^= bitmask;
-        self.state & bitmask != 0
-    }
-
-    fn are_inputs_all_high(&self, name: &str) -> bool {
-        let inputs = self.inputs.get(name).unwrap();
-        inputs.iter().all(|name| self.get_state(name))
-    }
-}
-
-#[derive(Debug)]
-struct Configuration {
-    modules: HashMap<String, Module>,
-    ids: HashMap<String, usize>,
-    inputs: HashMap<String, HashSet<String>>,
-}
-
-impl Configuration {
-    fn load(file: &str) -> Self {
-        fn name(token: &str) -> (String, ModuleType) {
-            match token.chars().next().unwrap() {
-                '%' => (token[1..].to_string(), ModuleType::FlipFlop),
-                '&' => (token[1..].to_string(), ModuleType::Conjunction),
-                _ => (token[0..].to_string(), ModuleType::Broadcaster),
-            }
-        }
-        fn outputs(token: &str) -> Vec<String> {
-            token.split(", ").map(|s| s.to_string()).collect()
-        }
-        let mut state_id: usize = 0;
-        let mut config = Configuration {
-            modules: HashMap::new(),
-            ids: HashMap::new(),
-            inputs: HashMap::new(),
-        };
-        for line in load::<String>(file) {
-            let tokens = line.split(" -> ").collect::<Vec<_>>();
-            let (name, mt) = name(tokens[0]);
-            let outputs = outputs(tokens[1]);
-            // set states ID
-            config.add_state_id(&mut state_id, &name);
-            // build reverse mapping
-            outputs.iter().for_each(|o| {
-                config
-                    .inputs
-                    .entry(o.to_string())
-                    .or_insert_with(HashSet::new)
-                    .insert(name.to_string());
-            });
-            // create module
-            config
-                .modules
-                .insert(name.to_string(), Module { name, outputs, mt });
-        }
-        config
-    }
-
-    fn init_state(&self) -> State {
-        let mut s = HashMap::new();
-        self.modules.iter().for_each(|(name, _)| {
-            s.insert(name.to_string(), 0);
-        });
-        State { s }
-    }
-
-    fn add_state_id(&mut self, id: &mut usize, name: &str) {
-        if !self.ids.contains_key(name) {
-            self.ids.insert(name.to_string(), *id);
-            *id += 1;
-        }
-    }
-
-    fn pulse(&self, state: &mut State, pulse: Pulse) -> Vec<Pulse> {
-        match self.modules.get(pulse.dst) {
-            None => vec![],
-            Some(m) => m.pulse( state, pulse),
-        }
-    }
-
-    fn pulse_until_steady(&self, pulse: Pulse) {
-        let mut state = self.init_state();
-        let mut pulses = vec![pulse];
-        loop {
-            println!("{:?} /// {:?}", state, pulses);
-            let mut out = vec![];
-            loop {
-                match pulses.pop() {
-                    None => break,
-                    Some(p) => out.append(&mut self.pulse(&mut state, p)),
+        pulses
+            .iter()
+            .for_each(|p| if p.high { hi += 1 } else { lo += 1 });
+        let mut new_pulses = pulses
+            .iter()
+            .flat_map(|p| {
+                // println!("{}", p);
+                match mods.get_mut(&p.dst_mask) {
+                    Some(m) => m.receive(&p),
+                    None => vec![],
                 }
-            }
-            if out.is_empty() {
+            })
+            .collect_vec();
+        // println!("\t{:?}", new_pulses);
+        pulses.clear();
+        pulses.append(&mut new_pulses);
+    }
+    (lo, hi)
+}
+
+fn repeat_until_all_low(broadcaster: u64, mut modules: Modules) -> (usize, usize, usize) {
+    let (mut lo, mut hi) = (0, 0);
+    let mut it = 0;
+    loop {
+        it += 1;
+        let (nlo, nhi) = push_button(&mut modules, broadcaster);
+        lo += nlo;
+        hi += nhi;
+        let all_low = modules.values().all(|m| match m.mt {
+            ModuleType::Broadcaster => true,
+            ModuleType::FlipFlop(state) => state == false,
+            ModuleType::Conjunction(state) => state == 0,
+            ModuleType::Rx(_) => true,
+        });
+        if all_low {
+            break;
+        }
+        if it == 1000 {
+            println!("Reached 1000 iterations");
+            break;
+        }
+    }
+    (it, lo, hi)
+}
+
+fn repeat_until_rx_low(broadcaster: u64, rx: u64, mut modules: Modules) -> usize {
+    let mut it = 0;
+    loop {
+        it += 1;
+        let (_, _) = push_button(&mut modules, broadcaster);
+        if let ModuleType::Rx(cnt) = modules.get(&rx).unwrap().mt {
+            if cnt > 0 {
                 break;
             }
-            pulses.append(&mut out);
+        }
+        if it % 1000 == 0 {
+            println!("Running for {} iterations...", it);
         }
     }
+    it
 }
 
 pub fn part1() -> usize {
-    let config = Configuration::load("data/test.txt");
-    config.pulse_until_steady(Pulse {
-        pt: PulseType::LOW,
-        src: "button",
-        dst: "broadcaster",
-    });
-    // FIXME call this 1000 times
-    0
+    let (bc, _, modules) = input("data/day20.txt");
+    let (it, lo, hi) = repeat_until_all_low(bc, modules);
+    println!(
+        "Stopped after {} iterations with (lo,hi) = ({},{})",
+        it, lo, hi
+    );
+    (1000 / it).pow(2) * (lo * hi)
 }
 
 pub fn part2() -> usize {
-    0
+    let (bc, rx, modules) = input("data/day20.txt");
+    repeat_until_rx_low(bc, rx, modules)
 }
 
 #[cfg(test)]
@@ -213,7 +297,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), 0);
+        assert_eq!(part1(), 777666211);
     }
 
     #[test]
